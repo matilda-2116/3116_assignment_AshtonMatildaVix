a3c1686e31ae8b64e294892f0b9762fc6a77ec6b
matilda-2116 <matilda.stewart07@gmail.com>
2026-09-25 13:52:34 +1000

    Import of data from moodle

---
+++ Starter file.py
@@ -0,0 +1,7 @@
+import pandas as pd #import pandas as a useful tool for dealing with csv files
+harris1 = pd.read_csv('HarrisPartI.csv') 
+harris3 = pd.read_csv('HarrisPartIII.csv')
+Krause21 = pd.read_csv('Krause21.csv')
+Vdb = pd.read_csv('vandenBerg_table2.csv')
+# this is an import of all the data on moodle for the Option 1 assignment. 
+#we can start working on this data now once we have done research and planned our approach
